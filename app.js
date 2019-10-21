df857af6837ec75355777cda22ad3ed438bc06a1
hc2 <eliel.floyd@bol.com.br>
2019-10-21 07:23:22 -0300

    atualizando com todos os indicadores

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-'use strict'
 const mongoose = require('mongoose')
 const Float = require('mongoose-float').loadType(mongoose)
 const fs = require('fs')
 const csv = require('fast-csv')
+const chokidar = require('chokidar');
 const express = require('express')
 const app = express()
 
@@ -26,9 +26,37 @@ const sellModel = mongoose.model('sell', new mongoose.Schema({
     oc: String
 }))
 
+const watcher = chokidar.watch('data', {ignored: /^\./, persistent: true});
+
+const end_timeout = 30000;
+
+watcher
+    .on('add', function(path) {
+
+        console.log('File', path, 'has been added');
+
+        fs.stat(path, function (err, stat) {
+            // Replace error checking with something appropriate for your app.
+            if (err) throw err;
+            setTimeout(checkEnd, end_timeout, path, stat);
+        });
+});
+
+function checkEnd(path, prev) {
+    fs.stat(path, function (err, stat) {
+
+        // Replace error checking with something appropriate for your app.
+        if (err) throw err;
+        if (stat.mtime.getTime() === prev.mtime.getTime()) {
+
+        }
+        else
+            setTimeout(checkEnd, end_timeout, path, stat);
+    });
+}
 const calculateSellQuantity = (qntCX, qtdUND, qntUN, retCX, retUN) => {
-    const total_qnt = (parseFloat(qntCX).toFixed(2) * parseFloat(qtdUND).toFixed(2) + parseFloat(qntUN).toFixed(2)) / parseFloat(qtdUND).toFixed(2)
-    const total_ret = (parseFloat(retCX).toFixed(2) * parseFloat(qtdUND).toFixed(2) + parseFloat(retUN).toFixed(2)) / parseFloat(qtdUND).toFixed(2)
+    const total_qnt = (parseInt(qntCX) * parseInt(qtdUND) + parseInt(qntUN)) / parseInt(qtdUND)
+    const total_ret = (parseInt(retCX) * parseInt(qtdUND) + parseInt(retUN)) / parseInt(qtdUND)
     return total_qnt - total_ret
 }
 
@@ -45,108 +73,116 @@ const toDate = (dateStr) => {
 
 const removeDuplcates = (arr) => arr.filter((este, i) => arr.indexOf(este) === i)
 
-if(checkVolume_600_300_1lt(pedido)) volume_300_600_1lt += pedido.sell_quant
-if(checkVolume_premium(pedido)) volume_premium += pedido.sell_quant
-if(checkVolume_volume_total(pedido)) volume_volume_total += pedido.sell_quant
-if(checkKVolume_curinga01(pedido)) volume_curinga01 += pedido.sell_quant
-if(checkKVolume_curinga02(pedido)) volume_curinga02 += pedido.sell_quant
-
-const checkVolume_600_300_1lt = (sell) => {
+const checkVolume_curinga02 = (sell) => {
     let isValid = false
-    const fam = ['001','004','025','061','062','081']
-    fam.map(element => { if(element === sell.fam_prod) {isValid = true} })
+    const fam = ['0000902411', '0000902852', '0000902410', '0000902406', '0000902451']
+    fam.map(element => { if (element === sell.cod_prod) { isValid = true } })
+    return isValid
+}
+
+const checkVolume_curinga01 = (sell) => {
+    let isValid = false
+    const fam = ['0000903061', '0000903126', '0000901161', '0000903131', '0000903129', '0000903130']
+    fam.map(element => { if (element === sell.cod_prod) { isValid = true } })
     return isValid
 }
 
 const checkVolume_premium = (sell) => {
     let isValid = false
-    const fam = ['010','081','025','061','062','081']
-    fam.map(element => { if(element === sell.fam_prod) {isValid = true} })
+    const fam = ['010', '081', '040', '004']
+    fam.map(element => {
+        if (element === sell.fam_prod || '0000903750' === sell.cod_prod) {
+            isValid = true
+        }
+        if ('0000901161' === sell.cod_prod) {
+            isValid = false
+        }
+    })
+    return isValid
+}
+
+const checkVolume_600_300_1lt = (sell) => {
+    let isValid = false
+    const fam = ['001', '004', '025', '061', '062', '081']
+    fam.map(element => { if (element === sell.fam_prod) { isValid = true } })
     return isValid
 }
 
 const checkCoberturaDevassa600 = (sell) => {
     let isValid = false
     const fam = ['0000903061']
-    fam.map(element => { if(element === sell.cod_prod) {isValid = true} })
+    fam.map(element => { if (element === sell.cod_prod) { isValid = true } })
     return isValid
 }
 
 const checkCoberturaDevassaTotal = (sell) => {
     let isValid = false
-    const fam = ['0000903061','0000901161','0000903062', '0000903130', '0000903129']
-    fam.map(element => { if(element === sell.cod_prod) {isValid = true} })
+    const fam = ['0000903061', '0000901161', '0000903062', '0000903130', '0000903129']
+    fam.map(element => { if (element === sell.cod_prod) { isValid = true } })
     return isValid
 }
 
 const checkCoberturaEisenbahn600 = (sell) => {
     let isValid = false
-    const fam = ['0000903024','0000903025']
-    fam.map(element => { if(element === sell.cod_prod) {isValid = true} })
+    const fam = ['0000903024', '0000903025']
+    fam.map(element => { if (element === sell.cod_prod) { isValid = true } })
     return isValid
 }
 
 const checkCoberturaEisenbahnTotal = (sell) => {
     let isValid = false
-    const fam = ['0000903024','0000901781','0000903212', '0000903561', '0000900820', '0000902241', '0000902544', '0000903025']
-    fam.map(element => { if(element === sell.cod_prod) {isValid = true} })
+    const fam = ['0000903024', '0000901781', '0000903212', '0000903561', '0000900820', '0000902241', '0000902544', '0000903025']
+    fam.map(element => { if (element === sell.cod_prod) { isValid = true } })
     return isValid
 }
 
 const checkCoberturaSchin600 = (sell) => {
     let isValid = false
     const fam = ['0000902411']
-    fam.map(element => { if(element === sell.cod_prod) {isValid = true} })
+    fam.map(element => { if (element === sell.cod_prod) { isValid = true } })
     return isValid
 }
 
 const checkCoberturaSchinTotal = (sell) => {
     let isValid = false
     const fam = ['0000902411', '0000902406', '0000902410', '0000902432', '0000902452', '0000902451']
-    fam.map(element => { if(element === sell.cod_prod) {isValid = true} })
+    fam.map(element => { if (element === sell.cod_prod) { isValid = true } })
     return isValid
 }
 
 const checkCoberturaNA = (sell) => {
     let isValid = false
     const fam = ['200', '210', '245', '255', '310', '360', '437', '033']
-    fam.map(element => { if(element === sell.fam_prod) {isValid = true} })
+    fam.map(element => { if (element === sell.fam_prod) { isValid = true } })
     return isValid
 }
 
 const checkCoberturaCervejas600 = (sell) => {
     let isValid = false
     const fam = ['001', '004', '081']
-    fam.map(element => { if(element === sell.fam_prod) {isValid = true} })
+    fam.map(element => { if (element === sell.fam_prod) { isValid = true } })
     return isValid
 }
 
 const checkCoberturaCervejasTotal = (sell) => {
     let isValid = false
     const fam = ['001', '003', '004', '010', '025', '030', '040', '060', '061', '081']
-    fam.map(element => { if(element === sell.fam_prod) {isValid = true} })
+    fam.map(element => { if (element === sell.fam_prod) { isValid = true } })
     return isValid
 }
 
-const checkSell = (sell) => {
-    let isValid = false
-    if(sell['Ocorr'] === '001' || sell['Ocorr'] === '002' || sell['Ocorr'] === '004') {
-        if(sell['Canc'] === '000' || sell['Canc'] === '008') {
-            if(sell['Cat Prod'] != '99') {
-                isValid = true
-            }
-        }
-    }
-
-    return isValid
-}
+const checkIfOcurrency = (sell) => (sell['Ocorr'] === '001' || sell['Ocorr'] === '002' || sell['Ocorr'] === '004') ? true : false
+const chechIfCancelled = (sell) => (sell['Canc'] === '000' || sell['Canc'] === '008') ? true : false
+const chechIfCategory = (sell) => (sell['Cat Prod'] != '99') ? true : false
+const checkSell = (sell) => (checkIfOcurrency(sell) && chechIfCancelled(sell) && chechIfCategory(sell)) ? true : false
 
 async function run() {
     try {
-        //await sellModel.deleteMany({})
+        
+        console.log('iniciando a importação')
 
-        fs.createReadStream('PEDIDOS.CSV')
-            .pipe(csv.parse({ delimiter: '', headers: true }))
+        fs.createReadStream('data/PEDIDOS.CSV')
+            .pipe(csv.parse({ delimiter: ';', headers: true }))
             .on('data', async (row) => {
                 try {
                     if (checkSell(row)) {
@@ -171,14 +207,13 @@ async function run() {
                     console.log('error: ', error)
                 }
             })
+            .on('end', () => console.log('importação finalizada com sucesso') )
     } catch (error) {
         console.log('deu error: ', error)
     }
 }
 
-//run()
-
-async function calculaFaturamento() {
+app.get('/get-data-by-seller/:vd', async (req, res) => {
     let faturamento = 0
     let volume_300_600_1lt = 0
     let volume_premium = 0
@@ -195,125 +230,61 @@ async function calculaFaturamento() {
     let clientesCobEisenbahnTotal = []
     let clientesCobDevassa600 = []
     let clientesCobDevassaTotal = []
+    let dados = {}
 
-    if ("101") {
-        const pedidos = await sellModel.find({ seller: "101" }).exec()
-        pedidos.map(pedido => {
-            faturamento += pedido.sell_value
-            if(pedido.sell_value > 0) clientesCobTotal.push(pedido.client)
-            if(pedido.sell_value > 0 && checkCoberturaCervejasTotal(pedido)) clientesCobCervejasTotal.push(pedido.client)
-            if(pedido.sell_value > 0 && checkCoberturaCervejas600(pedido)) clientesCobCervejas600.push(pedido.client) 
-            if(pedido.sell_value > 0 && checkCoberturaNA(pedido)) clientesCobNA.push(pedido.client)
-            if(pedido.sell_value > 0 && checkCoberturaSchin600(pedido)) clientesCobSchin600.push(pedido.client)
-            if(pedido.sell_value > 0 && checkCoberturaSchinTotal(pedido)) clientesCobSchinTotal.push(pedido.client)
-            if(pedido.sell_value > 0 && checkCoberturaEisenbahn600(pedido)) clientesCobEisenbahn600.push(pedido.client)
-            if(pedido.sell_value > 0 && checkCoberturaEisenbahnTotal(pedido)) clientesCobEisenbahnTotal.push(pedido.client)
-            if(pedido.sell_value > 0 && checkCoberturaDevassa600(pedido)) clientesCobDevassa600.push(pedido.client)
-            if(pedido.sell_value > 0 && checkCoberturaDevassaTotal(pedido)) clientesCobDevassaTotal.push(pedido.client)
-
-            if(checkVolume_600_300_1lt(pedido)) volume_300_600_1lt += pedido.sell_quant
-            if(checkVolume_premium(pedido)) volume_premium += pedido.sell_quant
-            if(checkVolume_volume_total(pedido)) volume_volume_total += pedido.sell_quant
-            if(checkKVolume_curinga01(pedido)) volume_curinga01 += pedido.sell_quant
-            if(checkKVolume_curinga02(pedido)) volume_curinga02 += pedido.sell_quant
-        })
-
-        const dados = {
-            faturamento:parseFloat(faturamento).toFixed(2),
-            clientesCobTotal:removeDuplcates(clientesCobTotal),
-            clientesCobCervejasTotal:removeDuplcates(clientesCobCervejasTotal),
-            clientesCobCervejas600:removeDuplcates(clientesCobCervejas600),
-            clientesCobNA: removeDuplcates(clientesCobNA),
-            clientesCobSchin600: removeDuplcates(clientesCobSchin600),
-            clientesCobSchinTotal: removeDuplcates(clientesCobSchinTotal),
-            clientesCobEisenbahn600: removeDuplcates(clientesCobEisenbahn600),
-            clientesCobEisenbahnTotal: removeDuplcates(clientesCobEisenbahnTotal),
-            clientesCobDevassa600:removeDuplcates(clientesCobDevassa600),
-            clientesCobDevassaTotal:removeDuplcates(clientesCobDevassaTotal)
-        }
-        
-        console.log(clientesCobTotal)
-    }
-}
-
-calculaFaturamento()
-/*
-app.get('/get-fund-com-by-vd/:vd', async(req, res) => {
-    let faturamento = 0
-    if(req.params.vd) {
-        const pedidos = await sellModel.find({seller:req.params.vd}).exec()
-        pedidos.map(elemento => {
-            faturamento += elemento.sell_value
-        })
-        console.log(faturamento)
-    }
-})
-*/
-/*
-app.get('/import', async (req, res) => {
-    await run()
-    return res.json({ message: 'importação finalizada com sucesso' })
-})
-
-app.get('/getRevenuesBySeller/:codSeller', async (req, res) => {
-    const value = await sellModel.aggregate([
-        {
-            $match: { $or: [{ status: '0' }, { status: '8' }] }
-        }
-    ]).exec()
-    console.log('params:', req.params)
-})
-
-async function load(seller_code) {
     try {
-        const value = await sellModel.aggregate([
-            {
-                $match: {
-                    $and: [
-                        { 'status': { $in: [0, 8] } },
-                        { 'oc': { $in: ['001', '002', '004'] } },
-                        //{'seller':seller_code}
-                    ]
-                }
-            }, 
-            {
-                $group: {
-                    _id: '$seller',
-                    fat: {
-                        $sum: '$sell_value'
-                    },
-                    totalCoverage: {
-                        $addToSet: '$client'
-                    },
-                    schinCoverage: {
-                        $sum: {
-                            $cond: [
-                                {$eq: ['$cod_prod','902411']},
-                                1,
-                                0
-                            ]
-                        }
-                    }
-                    
-                }
-            },
-            {
-                $project: {
-                    _id:0,
-                    vendedor:"$_id",
-                    valor_faturamento:"$fat",
-                    cobertura_total:"$totalCoverage",
-                    cobertura_schin:"$schinCoverage"
-                }
-            }
-        ]).exec()
-        console.log('params:', value)
-    } catch (error) {
-        console.log('error', error)
-    }
-}
 
-*/
+        if (req.params.vd) {
+            const pedidos = await sellModel.find({ seller: req.params.vd }).exec()
+            pedidos.map(pedido => {
+                faturamento += pedido.sell_value
+                if (pedido.sell_value > 0) clientesCobTotal.push(pedido.client)
+                if (pedido.sell_value > 0 && checkCoberturaCervejasTotal(pedido)) clientesCobCervejasTotal.push(pedido.client)
+                if (pedido.sell_value > 0 && checkCoberturaCervejas600(pedido)) clientesCobCervejas600.push(pedido.client)
+                if (pedido.sell_value > 0 && checkCoberturaNA(pedido)) clientesCobNA.push(pedido.client)
+                if (pedido.sell_value > 0 && checkCoberturaSchin600(pedido)) clientesCobSchin600.push(pedido.client)
+                if (pedido.sell_value > 0 && checkCoberturaSchinTotal(pedido)) clientesCobSchinTotal.push(pedido.client)
+                if (pedido.sell_value > 0 && checkCoberturaEisenbahn600(pedido)) clientesCobEisenbahn600.push(pedido.client)
+                if (pedido.sell_value > 0 && checkCoberturaEisenbahnTotal(pedido)) clientesCobEisenbahnTotal.push(pedido.client)
+                if (pedido.sell_value > 0 && checkCoberturaDevassa600(pedido)) clientesCobDevassa600.push(pedido.client)
+                if (pedido.sell_value > 0 && checkCoberturaDevassaTotal(pedido)) clientesCobDevassaTotal.push(pedido.client)
+
+                volume_volume_total += pedido.sell_quant
+                if (checkVolume_600_300_1lt(pedido)) volume_300_600_1lt += pedido.sell_quant
+                if (checkVolume_premium(pedido)) volume_premium += pedido.sell_quant
+                if (checkVolume_curinga01(pedido)) volume_curinga01 += pedido.sell_quant
+                if (checkVolume_curinga02(pedido)) volume_curinga02 += pedido.sell_quant
+            })
+
+            dados = {
+                faturamento: parseFloat(faturamento).toFixed(2),
+                clientesCobTotal: removeDuplcates(clientesCobTotal).length,
+                clientesCobCervejasTotal: removeDuplcates(clientesCobCervejasTotal).length,
+                clientesCobCervejas600: removeDuplcates(clientesCobCervejas600).length,
+                clientesCobNA: removeDuplcates(clientesCobNA).length,
+                clientesCobSchin600: removeDuplcates(clientesCobSchin600).length,
+                clientesCobSchinTotal: removeDuplcates(clientesCobSchinTotal).length,
+                clientesCobEisenbahn600: removeDuplcates(clientesCobEisenbahn600).length,
+                clientesCobEisenbahnTotal: removeDuplcates(clientesCobEisenbahnTotal).length,
+                clientesCobDevassa600: removeDuplcates(clientesCobDevassa600).length,
+                clientesCobDevassaTotal: removeDuplcates(clientesCobDevassaTotal).length,
+                volume_300_600_1lt,
+                volume_premium,
+                volume_volume_total,
+                volume_curinga01,
+                volume_curinga02
+            }
+
+        }
+        return res.json(dados)
+    } catch (error) {
+        console.log('error: ', error)
+    }
+})
+
+// app.get('/import', async (req, res) => {
+//     await run(res)
+// })
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
